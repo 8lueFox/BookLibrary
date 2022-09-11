551df19d1aace5486ba46b143365717d80ced8a8
Kacper Jędrzejewski <Kjedrzejewski@sygnity.pl>
2022-09-11 12:23:53 +0200

    add service

--- BookLibrary/Program.cs
+++ BookLibrary/Program.cs
@@ -1,7 +1,19 @@
+using BookLibrary.Services;
+using ServiceReference1;
+using SoapCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+builder.Services.AddSoapCore();
+builder.Services.AddSingleton<BookService, BooksService>();
+builder.Services.AddMvc();
 
+var app = builder.Build();
 app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.UseSoapEndpoint<BookService>("/BookService.svc", new SoapEncoderOptions(), SoapSerializer.XmlSerializer);
+});
 
 app.Run();
---
+++ BookLibrary/Services/BooksService.cs
@@ -0,0 +1,21 @@
+﻿using ServiceReference1;
+
+namespace BookLibrary.Services;
+
+public class BooksService : BookService
+{
+    public Task<AddBookResponse> AddBookAsync(AddBookRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetAllBooksResponse> GetAllBooksAsync(GetAllBooksRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetBookResponse> GetBookAsync(GetBookRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
